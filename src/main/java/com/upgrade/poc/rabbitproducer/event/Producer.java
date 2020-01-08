cb62df4ad0e774250ad2e0173c6b88b74d82a8d1
Tomas Juarez <tjuarez@upgrade.com>
2020-01-08 17:45:44 -0300

    Add message basic properties

--- src/main/java/com/upgrade/poc/rabbitproducer/event/Producer.java
+++ src/main/java/com/upgrade/poc/rabbitproducer/event/Producer.java
@@ -2,12 +2,12 @@ package com.upgrade.poc.rabbitproducer.event;
 
 import com.upgrade.poc.rabbitproducer.model.SpectrumMessage;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.amqp.core.AmqpTemplate;
-import org.springframework.amqp.core.Message;
 import org.springframework.amqp.core.Queue;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
+
+import java.util.Random;
 
 @Slf4j
 @Component
@@ -16,6 +16,7 @@ public class Producer {
     private RabbitTemplate rabbitTemplate;
 
     private Queue queue;
+
 
     public Producer(RabbitTemplate template, Queue queue) {
         this.rabbitTemplate = template;
@@ -28,7 +29,15 @@ public class Producer {
         message.setAmount("$125.00");
         message.setTransaction("PAYMENT");
 
-        rabbitTemplate.convertAndSend(queue.getName(), message);
+        Random random = new Random();
+        int randomId = random.ints(0, (14678)).findFirst().getAsInt();
+
+        rabbitTemplate.convertAndSend(queue.getName(), message, m -> {
+            m.getMessageProperties().setMessageId(String.valueOf(randomId));
+            m.getMessageProperties().setContentEncoding("utf-8");
+            m.getMessageProperties().setContentType("application/json");
+            return m;
+        });
 
         log.info("Sending spectrum message={}, queue={}", message, queue.getName());
     }
--- src/main/java/com/upgrade/poc/rabbitproducer/model/SpectrumMessage.java
+++ src/main/java/com/upgrade/poc/rabbitproducer/model/SpectrumMessage.java
@@ -1,7 +1,5 @@
 package com.upgrade.poc.rabbitproducer.model;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import lombok.Data;
 import lombok.ToString;
 
@@ -12,5 +10,4 @@ public class SpectrumMessage {
     private String transaction;
 
     private String amount;
-
 }
